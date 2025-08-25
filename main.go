d97d8940cb8faea289f186e7f9aa80da11a53716
Lakshya Saharan <lakshyasaharan@gmail.com>
2025-08-25 19:43:33 -0400

    feat: get hold of goroutines id, parent id, start and end times

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	exptrace "golang.org/x/exp/trace"
 	gotraceui "honnef.co/go/gotraceui/trace/ptrace"
@@ -26,7 +28,32 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Find earliest start time among goroutines
+	var startTs exptrace.Time
+	first := true
 	for _, g := range tr.Goroutines {
-		fmt.Printf("ID: %d ParentID: %d Start: %v, End: %v\n", g.ID, g.Parent, g.EffectiveStart(), g.EffectiveEnd())
+		s := g.EffectiveStart()
+		if first || s < startTs {
+			startTs = s
+			first = false
+		}
 	}
+
+	for _, g := range tr.Goroutines {
+
+		if g.Function == nil {
+			continue
+		}
+
+		// Only keep goroutines from "main" package
+		if !strings.HasPrefix(g.Function.Func, "main.") {
+			continue
+		}
+
+		s := time.Duration(g.EffectiveStart() - startTs)
+		e := time.Duration(g.EffectiveEnd() - startTs)
+		fmt.Printf("ID %d, ParentID %d start=%v, end=%v, duration=%v\n",
+			g.ID, g.Parent, s, e, e-s)
+	}
+
 }
