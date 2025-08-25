cdc44b48bac9d45eb4bdfdf351cf2949d8486f25
Lakshya Saharan <lakshyasaharan@gmail.com>
2025-08-25 18:19:54 -0400

    feat: use gotraceui to parse trace.out and get hold of goroutines data

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
 
-func main(){
-	fmt.Println("This is concurrency visualiser")
+	exptrace "golang.org/x/exp/trace"
+	gotraceui "honnef.co/go/gotraceui/trace/ptrace"
+)
+
+func main() {
+	f, err := os.Open("trace.out")
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	r, err := exptrace.NewReader(f)
+	if err != nil {
+		return
+	}
+
+	tr, err := gotraceui.Parse(r, func(float64) {})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, g := range tr.Goroutines {
+		fmt.Printf("ID: %d ParentID: %d Start: %v, End: %v\n", g.ID, g.Parent, g.EffectiveStart(), g.EffectiveEnd())
+	}
 }
