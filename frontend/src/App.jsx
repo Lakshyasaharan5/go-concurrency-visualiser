4c5003e9443973fd9614f85d4e01166ae628c5ae
Lakshya Saharan <lakshyasaharan@gmail.com>
2025-08-31 01:53:55 -0400

    feat(frontend): create divs using JSON tree
    It will parse the JSON tree into divs using recursive dfs
    Each div splits into child div in equal proportion
    This is initial step of logic to display Goroutine threads

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,70 @@
 export default function App() {
   return <>
-    
+    <Tree node={data} />
   </>
+}
+
+function Tree({ node }) {
+  if (!node){
+    return null;
+  }
+  return <div style={{ border: "1px solid darkblue", flex: 1, display: "flex", height: "100vh" }}>
+    { node.children.map((child) => {
+      return <Tree key={child.id} node={child} />
+    }) }
+  </div>
+}
+
+const data = {
+  "id": 1,
+  "children": [
+    {
+      "id": 2,
+      "children": [
+        {
+          "id": 3,
+          "children": []
+        },
+        {
+          "id": 4,
+          "children": [
+            {
+              "id": 5,
+              "children": []
+            },
+            {
+              "id": 6,
+              "children": [
+                {
+                  "id": 7,
+                  "children": []
+                },
+                {
+                  "id": 8,
+                  "children": []
+                }
+              ]
+            },
+            {
+              "id": 9,
+              "children": []
+            }
+          ]
+        }
+      ]
+    },    
+    {
+      "id": 10,
+      "children": [
+        {
+          "id": 11,
+          "children": []
+        },
+        {
+          "id": 12,
+          "children": []
+        }
+      ]
+    }
+  ]
 }
